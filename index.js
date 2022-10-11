0d8520c8dd70c93cd6597bb1b4532bae6450e49b
SvetoslavSO <sshaunin@mail.ru>
2022-10-11 21:00:53 +0300

    homework 2 corrected

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const http = require('http')
+const yargs = require('yargs')
 
-process.env.INTERVAL = 2000;
-process.env.TIMEOUT = 20000;
+const args = yargs
+  .usage('Usage: node $0 [options]')
+  .help('help')
+  .alias('help', 'h')
+  .version('1.0.0')
+  .alias('version', 'v')
+  .option('interval', {
+    alias: 'i',
+    describe: 'interval time',
+    default: '2000'
+  })
+  .option('timeout', {
+    alias: 't',
+    describe: 'timeout time',
+    default: '20000'
+  })
+  .argv
 
-const interval = process.env.INTERVAL
-const timeout = process.env.TIMEOUT
+const config = {
+  interval: args.interval,
+  timeout: args.timeout
+}
+
+process.env.INTERVAL = config.interval;
+process.env.TIMEOUT = config.timeout;
 
 function getCurrentDate () {
   let today = new Date()
@@ -21,19 +42,21 @@ function getCurrentDate () {
 let tick = 0;
 
 const server = http.createServer((req, res) => {
+  const tickNumber = Math.floor(process.env.TIMEOUT / process.env.INTERVAL) - 1
+  console.log(tickNumber)
   const currentInterval = setInterval(() => {
-    if(tick < 18){
-      tick = tick + 2;
+    if(tick <= tickNumber){
+      tick = tick + 1;
       let currentDate = getCurrentDate()
       console.log(currentDate)
     }
-  }, interval)
+  }, process.env.INTERVAL)
   const currentTimeout = setTimeout(() => {
     let currentDate = getCurrentDate()
     clearInterval(currentInterval);
     clearTimeout(currentTimeout)
     res.end(currentDate)
-  }, timeout)
+  }, process.env.TIMEOUT)
   if(req.method === 'GET') {
     currentInterval;
     currentTimeout
